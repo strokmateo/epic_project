77b7c6716be3e84bcd7a13abb87a01a21a7d965f
mstrok <strok.mateo@outlook.com>
2024-12-10 16:22:31 +0100

    Add TokenProviderService and it's interface

---
+++ Backend/Services/TokenProviderService.cs
@@ -0,0 +1,30 @@
+﻿using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+using Backend.Entities;
+using Backend.Services.Interfaces;
+using System.IdentityModel.Tokens;
+
+namespace Backend.Services
+{
+    internal sealed class TokenProviderService(IConfiguration configuration) : ITokenProviderService
+    {
+        public string GenerateToken(User user)
+        {
+            string secretKey = configuration["Jwt:Secret"]!;
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey));
+        
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var tokenDescriptor = new SecurityTokenDescriptor 
+            {
+                Subject = new ClaimsIdentity(
+                    [
+                        new Claim(JwtRegisteredClaimNames.Sub, user.Id.ToString(),
+                        new Claim(JwtRegisteredClaimNames.Email, user.Email),
+                        new Claim("email_verified", user.EmailVerified.Tostring())
+                    ])
+            }
+        }
+    }
+}
