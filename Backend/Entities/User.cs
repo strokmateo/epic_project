27b01747b209fb66c08ba6d953bba941365715aa
mstrok <strok.mateo@outlook.com>
2024-12-10 16:21:18 +0100

    Add IsEmailVerified prop and method check in User entity

--- Backend/Entities/User.cs
+++ Backend/Entities/User.cs
@@ -6,4 +6,16 @@ public class User
     public string UserName { get; set; }
     public string PasswordHash { get; set; }
     public string Email { get; set; }
-}
+    public bool EmailVerified { get; set; }
+
+    public string IsEmailVerified()
+    {
+        if (EmailVerified)
+        {
+            return "true";
+        }
+        else
+        {
+            return "false";
+        }   
+    }
