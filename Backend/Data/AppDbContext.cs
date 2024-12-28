8bcf5d3e24dcf1babcd96acf91ffd3f6490e7b58
mstrok <strok.mateo@outlook.com>
2024-12-28 22:58:05 +0100

    Extend AppDbContext to IdentityDbContext

--- Backend/Data/AppDbContext.cs
+++ Backend/Data/AppDbContext.cs
@@ -1,14 +1,14 @@
 ﻿using Backend.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Backend.Data
 {
-    public class AppDbContext : DbContext
+    public class AppDbContext : IdentityDbContext<User, IdentityRole<Guid>, Guid>
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
         }
-
-        public DbSet<User> Users { get; set; }
     }
 }
