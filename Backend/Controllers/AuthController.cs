c5f7790b0c732fef593f4af7b2deb555d6d64099
mstrok <strok.mateo@outlook.com>
2024-12-28 23:11:24 +0100

    Add Login and Register action methods to AuthController

--- Backend/Controllers/AuthController.cs
+++ Backend/Controllers/AuthController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Backend.Models;
+using Backend.Models.LoginAuthModels;
+using Backend.Services;
+using Backend.Services.Interfaces;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Backend.Controllers
@@ -7,9 +11,48 @@ namespace Backend.Controllers
     [ApiController]
     public class AuthController : ControllerBase
     {
-        public Task<IActionResult> LogIn()
-        {
+        private readonly IAuthService _authService;
+        private readonly IUserService _userService;
+        private readonly ITokenProviderService _tokenProviderService;
 
+        public AuthController(IAuthService authService, ITokenProviderService tokenProviderService, IUserService userService)
+        {
+            _authService = authService;
+            _userService = userService;
+            _tokenProviderService = tokenProviderService;
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(LoginRequestDto loginDto)
+        {
+            var result = await _authService.AreValidCredentials(loginDto);
+
+            if (result.Succeeded)
+            {
+                var user = result.Data;
+                var username = user.UserName ?? string.Empty;
+                var email = user.Email ?? string.Empty;
+                var token = _tokenProviderService.GenerateToken(user);
+
+                var response = new LoginResponseDto(username, email, token);
+                return Ok(response);
+            }
+
+            return Unauthorized("Username/Password is incorrect.");
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(RegisterRequestDto registerDto)
+        {
+            var result = await _authService.Register(registerDto);
+
+            if(!result.Succeeded)
+            {
+                // return Conflict("Username or Email already taken.");
+                return Conflict(result.Message);
+            }
+
+            return Ok();
         }
     }
 }
