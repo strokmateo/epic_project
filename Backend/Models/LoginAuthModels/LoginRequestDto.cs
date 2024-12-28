ff56d684e92636dc8b2c095fd11eed5d07276613
mstrok <strok.mateo@outlook.com>
2024-12-28 23:03:18 +0100

    Add login DTO records

---
+++ Backend/Models/LoginAuthModels/LoginRequestDto.cs
@@ -0,0 +1,4 @@
+﻿namespace Backend.Models.LoginAuthModels
+{
+    public record LoginRequestDto(string EmailUsername, string Password);
+}
---
+++ Backend/Models/LoginAuthModels/LoginResponseDto.cs
@@ -0,0 +1,4 @@
+﻿namespace Backend.Models.LoginAuthModels
+{
+    public record LoginResponseDto(string Username, string Email, string Token);
+}
