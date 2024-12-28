5e2006414509bd45da1e78fc13ef73a9706f31e0
mstrok <strok.mateo@outlook.com>
2024-12-28 23:09:15 +0100

    Semantic changes to IRepository interface

--- Backend/Repositories/Interfaces/IRepository.cs
+++ Backend/Repositories/Interfaces/IRepository.cs
@@ -9,7 +9,7 @@ public interface IRepository<TEntity> where TEntity : class
     Task<TEntity?> GetByIdAsync(Guid id);
     Task<IEnumerable<TEntity?>> GetAllAsync();
     Task<IEnumerable<TEntity>> FindAsync(Expression<Func<TEntity, bool>> predicate);
-    Task Add(TEntity entity);
+    Task AddAsync(TEntity entity);
     Task AddRangeAsync(IEnumerable<TEntity> entities);
     Task RemoveAsync(TEntity entity);
     Task RemoveRangeAsync(IEnumerable<TEntity> entities);
