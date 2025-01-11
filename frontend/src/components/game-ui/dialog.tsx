6c4350fac67531ef6d834308c22dbff1b05fed8f
mstrok <strok.mateo@outlook.com>
2025-01-11 02:25:35 +0100

    Add dialog component for npc output text

--- frontend/src/components/game-ui/dialog.tsx
+++ frontend/src/components/game-ui/dialog.tsx
@@ -1,8 +1,24 @@
-export default function Dialog() {
+import { cn } from "../../lib/utils";
+import TypewriterEffect from "./typeWriterEffect";
+
+export default function Dialog({
+    text,
+    className,
+}: {
+    text: string;
+    className?: string;
+}) {
     return (
-        <div>
-            This is a character speech dialog on bottom of screen (to be
-            implemented)
+        // currently only works for npc -> player output, todo is to add func for player -> npc input
+        <div
+            className={cn(
+                "bg-slate-300  w-screen h-[150px] animate-slide-up flex font-pixel text-[30px]",
+                className
+            )}
+        >
+            <div className="w-1/2 p-2">
+                <TypewriterEffect text={text} />
+            </div>
         </div>
     );
 }
