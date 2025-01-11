9d569e0232342561ab03ddbe54515aca880516dd
mstrok <strok.mateo@outlook.com>
2025-01-11 02:27:30 +0100

    Add characterDialog dialog+character display

---
+++ frontend/src/components/game-ui/characterDialog.tsx
@@ -0,0 +1,31 @@
+import { cn } from "../../lib/utils";
+import Character from "./character";
+import Dialog from "./dialog";
+
+export default function CharacterDialog({
+    characterSrc,
+    className,
+    text
+}: {
+    characterSrc: string;
+    className: string;
+    text: string
+}) {
+    return (
+        <div className={cn("relative", className)}>
+            {/* Dialog */}
+            <div className="relative z-10">
+                <Dialog text={text} />
+            </div>
+
+            {/* Character */}
+            <div className="absolute bottom-0 right-0 w-1/2 flex justify-center z-20 translate-y-28">
+                <Character
+                    imageSrc={characterSrc}
+                    scale={0.75}
+                    className="animate-slide-up-character"
+                />
+            </div>
+        </div>
+    );
+}
