04fd2075cc59d9b010282e34c18164c2a9fdc082
mstrok <strok.mateo@outlook.com>
2025-01-11 02:26:46 +0100

    Add typewrite component for text effect

---
+++ frontend/src/components/game-ui/typeWriterEffect.tsx
@@ -0,0 +1,40 @@
+import { useState, useEffect } from "react";
+
+export default function TypewriterEffect({
+    text,
+    speed = 50,
+    className = "",
+}: {
+    text: string;
+    speed?: number;
+    className?: string;
+}) {
+    const [displayedText, setDisplayedText] = useState("");
+    const [displayClickNext, setDisplayClickNext] = useState<boolean>(false);
+
+    useEffect(() => {
+        let currentIndex = 0;
+
+        const interval = setInterval(() => {
+            if (currentIndex < text.length) {
+                // Directly set the substring of the text
+                setDisplayedText(text.slice(0, currentIndex + 1));
+                currentIndex++;
+            } else {
+                setDisplayClickNext(true);
+                clearInterval(interval); // Stop the interval when done
+            }
+        }, speed);
+
+        return () => clearInterval(interval); // Clean up on component unmount
+    }, [text, speed]);
+
+    return (
+        <p className={className}>
+            {displayedText}
+            {displayClickNext && (
+                <span className="animate-fade"> Click to Continue</span>
+            )}
+        </p>
+    );
+}
