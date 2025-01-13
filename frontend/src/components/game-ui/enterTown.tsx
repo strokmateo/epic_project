8f1fc2fb3b5533a08568673e0efb80301f8d9c89
strokmateo <strok.mateo@outlook.com>
2025-01-13 20:24:43 +0100

    Fix merge error (hopefully)

--- frontend/src/components/game-ui/enterTown.tsx
+++ frontend/src/components/game-ui/enterTown.tsx
@@ -58,5 +58,63 @@ export default function EnterTown({ onClick }: { onClick: () => void }) {
         );
     }
 
+<<<<<<< Updated upstream
+=======
+  // Render element based on the current step
+  let renderElement: React.ReactElement = (
+    <div className="bg-black h-screen w-screen"></div>
+  );
+
+  if (currentEvent === "blackToPixelated") {
+    renderElement = (
+      <div className="bg-auth-background w-screen h-screen bg-cover opacity-0 animate-fade-in"></div>
+    );
+  }
+  if (currentEvent === "pixelatedToDark") {
+    renderElement = (
+      <div
+        onClick={onClick}
+        className="bg-auth-background w-screen h-screen bg-cover flex items-end relative overflow-hidden"
+      >
+        {/* Overlay */}
+        <div className="absolute inset-0 bg-black bg-opacity-50 animate-fade-in z-10"></div>
+        <CharacterDialog
+          className="z-30"
+          characterSrc="src/assets/images/town-guard-1.png"
+          text="Ho there, traveler! An unfamiliar face graces our gates. Who might ye be, and what business brings thee to our humble town?"
+        />
+      </div>
+    );
+  }
+
+    if (currentEvent === "blackToPixelated") {
+        renderElement = (
+            <div className="bg-auth-background w-screen h-screen bg-cover opacity-0 animate-fade-in"></div>
+        );
+    }
+    if (currentEvent === "pixelatedToDark") {
+        renderElement = (
+            <div
+                onClick={() => {
+                    setDialogVisibilty(false);
+                    setTimeout(() => {
+                        onClick();
+                    }, 500);
+                }}
+                className="bg-auth-background w-screen h-screen bg-cover flex items-end relative overflow-hidden"
+            >
+                {/* Overlay */}
+                <div className="absolute inset-0 bg-black bg-opacity-50 animate-fade-in z-10"></div>
+                <CharacterDialog
+                    className="z-30"
+                    characterSrc="src/assets/images/town-guard-1.png"
+                    text="Ho there, traveler! An unfamiliar face graces our gates. Who might ye be, and what business brings thee to our humble town?"
+                    dialogVisibility={dialogVisibilty}
+                />
+            </div>
+        );
+    }
+
+>>>>>>> Stashed changes
     return renderElement;
 }
