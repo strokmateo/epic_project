f09eefcbf973dded2019763a81393254cf953599
Matej2405 <123963920+Matej2405@users.noreply.github.com>
2025-01-30 20:07:17 +0100

    Added town hall intercation

--- frontend/src/pages/houses/TownHall.tsx
+++ frontend/src/pages/houses/TownHall.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import CharacterDialog from "../../components/game-ui/characterDialog";
 
 const TownHall: React.FC = () => {
     const navigate = useNavigate();
@@ -16,20 +17,27 @@ const TownHall: React.FC = () => {
         backgroundRepeat: "no-repeat",
     };
     return (
-        <div style={backgroundStyle}>
+        <div style={backgroundStyle } className="w-screen h-screen bg-cover flex flex-col justify-between relative overflow-hidden">
             <button
                 style={{ backgroundColor: "white" }}
                 onClick={() => navigate("/map")}
             >
                 Back
             </button>
+            
             <button
                 style={{ backgroundColor: "black", width: "150px" }}
-                className="fixed right-5 top-1/2 -translate-y-1/2 bg-blue-500 text-white px-4 py-2 rounded"
+                className="fixed right-5 top-1/2 -translate-y-1/2 bg-blue-500 text-white px-4 py-2 rounded z-40"
                 onClick={() => navigate("/leaderboard")}
             >
                 LEADERBOARD
             </button>
+            <CharacterDialog
+                            characterSrc="src/assets/images/rich-man-1.png"
+                            text={
+                                "Step away, nigga!!"
+                            }
+                        />  
         </div>
     );
 };
