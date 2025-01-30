94274534aa699ca59fdd75344f3fc323cd4beed8
Bruno Pjevalica <bruno.pjevalica1@gmail.com>
2025-01-30 20:05:55 +0100

    Finished hard-coded leaderboard

--- frontend/src/pages/leaderboard/Leaderboard.tsx
+++ frontend/src/pages/leaderboard/Leaderboard.tsx
@@ -20,7 +20,7 @@ const Leaderboard: React.FC = () => {
                 style={{
                     backgroundColor: "white",
                 }}
-                onClick={() => navigate("/map")}
+                onClick={() => navigate("/town-hall")}
             >
                 Back
             </button>
@@ -30,180 +30,134 @@ const Leaderboard: React.FC = () => {
                         LEADERBOARD
                     </h1>
                     <ul className="text-white text-3xl w-full text-left">
-                        <li className="flex justify-between items-center text-yellow-500 bg-black bg-opacity-50 m-2 p-2 pl-40 pr-10">
-                            {/* Ime igrača - poravnat na početak */}
-                            <span className="text-left w-1/3">pjevv</span>
-
-                            {/* XP i Coins u istom redu, poravnati na početak */}
-                            <span className="w-1/3 flex gap-x-8 text-left">
-                                {/* Coins */}
-                                <span className="flex">
-                                    <span className="text-white">Coins: </span>
-                                    <span className="text-red-500 ml-2">
-                                        130
-                                    </span>
-                                </span>
-
-                                {/* XP */}
-                                <span className="flex">
-                                    <span className="text-white">XP: </span>
-                                    <span className="text-blue-300 ml-2">
-                                        600
-                                    </span>
-                                </span>
+                        <li className="flex items-center bg-black bg-opacity-50 m-2 p-2 w-full">
+                            <span className="w-1/3 text-left text-yellow-500 pl-10">
+                                pjevv
+                            </span>
+                            <span className="w-1/3 flex text-left">
+                                <span className="text-white">Coins: </span>
+                                <span className="text-red-500 ml-2">130</span>
+                            </span>
+                            <span className="w-1/3 flex text-left">
+                                <span className="text-white">XP: </span>
+                                <span className="text-blue-300 ml-2">1272</span>
                             </span>
                         </li>
-                        <li className="flex justify-between items-center text-yellow-500 bg-black bg-opacity-50 m-2 p-2 pl-40 pr-10">
-                            <span>Matej2405</span>
-                            <span className="flex gap-x-8">
-                                <span>
-                                    <span className="text-white">Coins: </span>
-                                    <span className="text-red-500 pr-20">
-                                        120
-                                    </span>
-                                </span>
-                                <span>
-                                    <span className="text-white">XP: </span>
-                                    <span className="text-blue-300 pr-20">
-                                        570
-                                    </span>
-                                </span>
+                        <li className="flex items-center bg-black bg-opacity-50 m-2 p-2 w-full">
+                            <span className="w-1/3 text-left text-gray-400 pl-10">
+                                Matej240PET
+                            </span>
+                            <span className="w-1/3 flex text-left">
+                                <span className="text-white">Coins: </span>
+                                <span className="text-red-500 ml-2">120</span>
+                            </span>
+                            <span className="w-1/3 flex text-left">
+                                <span className="text-white">XP: </span>
+                                <span className="text-blue-300 ml-2">597</span>
                             </span>
                         </li>
-                        <li className="flex justify-between items-center text-yellow-500 bg-black bg-opacity-50 m-2 p-2 pl-40 pr-10">
-                            <span>MateoS</span>
-                            <span className="flex gap-x-8">
-                                <span>
-                                    <span className="text-white">Coins: </span>
-                                    <span className="text-red-500 pr-20">
-                                        260
-                                    </span>
-                                </span>
-                                <span>
-                                    <span className="text-white">XP: </span>
-                                    <span className="text-blue-300 pr-20">
-                                        548
-                                    </span>
-                                </span>
+                        <li className="flex items-center bg-black bg-opacity-50 m-2 p-2 w-full">
+                            <span className="w-1/3 text-left text-yellow-700 pl-10">
+                                MateoS
+                            </span>
+                            <span className="w-1/3 flex text-left">
+                                <span className="text-white">Coins: </span>
+                                <span className="text-red-500 ml-2">260</span>
+                            </span>
+                            <span className="w-1/3 flex text-left">
+                                <span className="text-white">XP: </span>
+                                <span className="text-blue-300 ml-2">548</span>
                             </span>
                         </li>
-                        <li className="flex justify-between items-center bg-black bg-opacity-20 m-2 p-2 pl-40 pr-10">
-                            <span>gobliN</span>
-                            <span className="flex gap-x-8">
-                                <span>
-                                    <span className="text-white">Coins: </span>
-                                    <span className="text-red-500 pr-20">
-                                        1479
-                                    </span>
-                                </span>
-                                <span>
-                                    <span className="text-white">XP: </span>
-                                    <span className="text-blue-300 pr-20">
-                                        547
-                                    </span>
-                                </span>
+                        <li className="flex items-center bg-black bg-opacity-20 m-2 p-2 w-full">
+                            <span className="w-1/3 text-left text-white pl-10">
+                                gobliN
+                            </span>
+                            <span className="w-1/3 flex text-left">
+                                <span className="text-white">Coins: </span>
+                                <span className="text-red-500 ml-2">1479</span>
+                            </span>
+                            <span className="w-1/3 flex text-left">
+                                <span className="text-white">XP: </span>
+                                <span className="text-blue-300 ml-2">544</span>
                             </span>
                         </li>
-                        <li className="flex justify-between items-center bg-black bg-opacity-20 m-2 p-2 pl-40 pr-10">
-                            <span>Player44</span>
-                            <span className="flex gap-x-8">
-                                <span>
-                                    <span className="text-white">Coins: </span>
-                                    <span className="text-red-500 pr-20">
-                                        44
-                                    </span>
-                                </span>
-                                <span>
-                                    <span className="text-white">XP: </span>
-                                    <span className="text-blue-300 pr-20">
-                                        499
-                                    </span>
-                                </span>
+                        <li className="flex items-center bg-black bg-opacity-20 m-2 p-2 w-full">
+                            <span className="w-1/3 text-left text-white pl-10">
+                                Player44
+                            </span>
+                            <span className="w-1/3 flex text-left">
+                                <span className="text-white">Coins: </span>
+                                <span className="text-red-500 ml-2">44</span>
+                            </span>
+                            <span className="w-1/3 flex text-left">
+                                <span className="text-white">XP: </span>
+                                <span className="text-blue-300 ml-2">499</span>
                             </span>
                         </li>
-                        <li className="flex justify-between items-center bg-black bg-opacity-20 m-2 p-2 pl-40 pr-10">
-                            <span>bestEver</span>
-                            <span className="flex gap-x-8">
-                                <span>
-                                    <span className="text-white">Coins: </span>
-                                    <span className="text-red-500 pr-20">
-                                        99
-                                    </span>
-                                </span>
-                                <span>
-                                    <span className="text-white">XP: </span>
-                                    <span className="text-blue-300 pr-20">
-                                        447
-                                    </span>
-                                </span>
+                        <li className="flex items-center bg-black bg-opacity-20 m-2 p-2 w-full">
+                            <span className="w-1/3 text-left text-white pl-10">
+                                bestEver
+                            </span>
+                            <span className="w-1/3 flex text-left">
+                                <span className="text-white">Coins: </span>
+                                <span className="text-red-500 ml-2">327</span>
+                            </span>
+                            <span className="w-1/3 flex text-left">
+                                <span className="text-white">XP: </span>
+                                <span className="text-blue-300 ml-2">446</span>
                             </span>
                         </li>
-                        <li className="flex justify-between items-center bg-black bg-opacity-20 m-2 p-2 pl-40 pr-10">
-                            <span>nigga</span>
-                            <span className="flex gap-x-8">
-                                <span>
-                                    <span className="text-white">Coins: </span>
-                                    <span className="text-red-500 pr-20">
-                                        852
-                                    </span>
-                                </span>
-                                <span>
-                                    <span className="text-white">XP: </span>
-                                    <span className="text-blue-300 pr-20">
-                                        384
-                                    </span>
-                                </span>
+                        <li className="flex items-center bg-black bg-opacity-20 m-2 p-2 w-full">
+                            <span className="w-1/3 text-left text-white pl-10">
+                                SwiftieEnjoyer77
+                            </span>
+                            <span className="w-1/3 flex text-left">
+                                <span className="text-white">Coins: </span>
+                                <span className="text-red-500 ml-2">332</span>
+                            </span>
+                            <span className="w-1/3 flex text-left">
+                                <span className="text-white">XP: </span>
+                                <span className="text-blue-300 ml-2">399</span>
                             </span>
                         </li>
-                        <li className="flex justify-between items-center bg-black bg-opacity-20 m-2 p-2 pl-40 pr-10">
-                            <span>ideGas999</span>
-                            <span className="flex gap-x-8">
-                                <span>
-                                    <span className="text-white">Coins: </span>
-                                    <span className="text-red-500 pr-20">
-                                        3
-                                    </span>
-                                </span>
-                                <span>
-                                    <span className="text-white">XP: </span>
-                                    <span className="text-blue-300 pr-20">
-                                        232
-                                    </span>
-                                </span>
+                        <li className="flex items-center bg-black bg-opacity-20 m-2 p-2 w-full">
+                            <span className="w-1/3 text-left text-white pl-10">
+                                ideGas999
+                            </span>
+                            <span className="w-1/3 flex text-left">
+                                <span className="text-white">Coins: </span>
+                                <span className="text-red-500 ml-2">3</span>
+                            </span>
+                            <span className="w-1/3 flex text-left">
+                                <span className="text-white">XP: </span>
+                                <span className="text-blue-300 ml-2">232</span>
                             </span>
                         </li>
-                        <li className="flex justify-between items-center bg-black bg-opacity-20 m-2 p-2 pl-40 pr-10">
-                            <span>skibidiHiljson</span>
-                            <span className="flex gap-x-8">
-                                <span>
-                                    <span className="text-white">Coins: </span>
-                                    <span className="text-red-500 pr-20">
-                                        342
-                                    </span>
-                                </span>
-                                <span>
-                                    <span className="text-white">XP: </span>
-                                    <span className="text-blue-300 pr-20">
-                                        130
-                                    </span>
-                                </span>
+                        <li className="flex items-center bg-black bg-opacity-20 m-2 p-2 w-full">
+                            <span className="w-1/3 text-left text-white pl-10">
+                                skibidiHiljson
+                            </span>
+                            <span className="w-1/3 flex text-left">
+                                <span className="text-white">Coins: </span>
+                                <span className="text-red-500 ml-2">900</span>
+                            </span>
+                            <span className="w-1/3 flex text-left">
+                                <span className="text-white">XP: </span>
+                                <span className="text-blue-300 ml-2">224</span>
                             </span>
                         </li>
-                        <li className="flex justify-between items-center bg-black bg-opacity-20 m-2 p-2 pl-40 pr-10">
-                            <span>mihael_glavicicic</span>
-                            <span className="flex gap-x-8">
-                                <span>
-                                    <span className="text-white">Coins: </span>
-                                    <span className="text-red-500 pr-20">
-                                        0
-                                    </span>
-                                </span>
-                                <span>
-                                    <span className="text-white">XP: </span>
-                                    <span className="text-blue-300 pr-20">
-                                        2
-                                    </span>
-                                </span>
+                        <li className="flex items-center bg-black bg-opacity-20 m-2 p-2 w-full">
+                            <span className="w-1/3 text-left text-white pl-10">
+                                mihael_glavicicic
+                            </span>
+                            <span className="w-1/3 flex text-left">
+                                <span className="text-white">Coins: </span>
+                                <span className="text-red-500 ml-2">0</span>
+                            </span>
+                            <span className="w-1/3 flex text-left">
+                                <span className="text-white">XP: </span>
+                                <span className="text-blue-300 ml-2">2</span>
                             </span>
                         </li>
                     </ul>
